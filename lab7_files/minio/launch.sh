cb4dea7835622579e57ea61168df876ec7aba159
Akshit Johry <akshitjohry@gmail.com>
2023-12-14 23:20:09 -0700

    Removing stale files

--- lab7_files/minio/launch.sh
+++
@@ -1,5 +0,0 @@
-helm repo add bitnami https://charts.bitnami.com/bitnami
-
-helm install -f ./minio-config.yaml -n minio-ns --create-namespace minio-proj bitnami/minio
-
-kubectl port-forward --namespace minio-ns svc/minio-proj 9001:9001 &
